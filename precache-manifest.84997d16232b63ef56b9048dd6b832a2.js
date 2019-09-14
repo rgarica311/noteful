dfc72b720966901376dfb0fffcb2edb485518a32
Rory Garcia <rory.garcia1@gmail.com>
2019-09-13 22:51:21 -0700

    Updates

---
+++ precache-manifest.84997d16232b63ef56b9048dd6b832a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2700bb5d03c2815160c8",
+    "url": "/rgarica311/noteful/static/js/runtime~main.2700bb5d.js"
+  },
+  {
+    "revision": "1428ecac7b8245479515",
+    "url": "/rgarica311/noteful/static/js/main.1428ecac.chunk.js"
+  },
+  {
+    "revision": "f3560386dbe188a96860",
+    "url": "/rgarica311/noteful/static/js/1.f3560386.chunk.js"
+  },
+  {
+    "revision": "1428ecac7b8245479515",
+    "url": "/rgarica311/noteful/static/css/main.8b27567c.chunk.css"
+  },
+  {
+    "revision": "c00368d17299e77502c8caac41823454",
+    "url": "/rgarica311/noteful/index.html"
+  }
+];
